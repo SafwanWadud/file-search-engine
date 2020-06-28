863d04bd45e27b53e1fe991ddfc1d7eaee3748d0
Safwan Wadud <safwan_wadud@hotmail.com>
2020-06-27 22:33:23 -0400

    implemented a simple web crawler
    implemented a simple web crawler given the pre-made webdev module

--- search_engine.py
+++ search_engine.py
@@ -1,4 +1,31 @@
+import webdev
+
 pageFreqCache = {}#nested dictionary cache to store all words and their frequencies for every page
+
+def crawlWeb():
+    canditates = []#contains URLs of pages that the web crawler has discovered and could read/save
+    crawled = []#contains URLs that the web crawler has already saved
+    maxPages = 5
+    outfile = open('pages.txt', 'w')
+
+    canditates.append("http://sikaman.dyndns.org:8888/courses/4601/resources/N-0.html")
+    while(len(canditates)!=0 and len(crawled) < maxPages):
+        currentURL = canditates.pop(0)
+        pageContent = webdev.readurl(currentURL)
+        linkedURLs = webdev.getlinks(pageContent)
+        for url in linkedURLs:
+            if url not in canditates and url not in crawled:
+                canditates.append(url)
+        pageContent = webdev.striphtml(pageContent)
+        filename = currentURL[currentURL.rfind('/')+1:]
+        outfile.write(filename+'\n')
+        out = open(filename,'w')
+        wordList = pageContent.split()
+        for word in wordList:
+            out.write(word+'\n')
+        out.close()
+        crawled.append(currentURL)
+    outfile.close()
 
 def mostFreq(freqDict):
     highestFreq = -1
@@ -29,7 +56,6 @@ def mostOccurences(pages, searchWord):
                     pageFreqCache[page][word]=1
                 else:
                     pageFreqCache[page][word]+=1
-
         if searchWord not in pageFreqCache[page]:
             pageFreq[page] = 0
             pageFreqRatio[page] = 0
@@ -40,12 +66,16 @@ def mostOccurences(pages, searchWord):
     mostFreqPage =  mostFreq(pageFreq)
     highestRatioPage =  mostFreq(pageFreqRatio)
 
-    print('Max page (Count): ', mostFreqPage)
-    print('Max Count: ', pageFreq[mostFreqPage])
-    print('Max page (Ratio): ', highestRatioPage)
-    print('Max Ratio: ', round(pageFreqRatio[highestRatioPage],4))
+    if(pageFreq[mostFreqPage]==0):
+        print('No matches found.')
+    else:
+        print('Max page (Count): ', mostFreqPage)
+        print('Max Count: ', pageFreq[mostFreqPage])
+        print('Max page (Ratio): ', highestRatioPage)
+        print('Max Ratio: ', round(pageFreqRatio[highestRatioPage],4))
 
 def main():
+    crawlWeb()
     infile = open('pages.txt','r')
     pages = infile.read().split('\n')
     pages.pop(-1)
